5c22c8407cebbfcd8b9791ff7673757d7751ae6b
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2023-12-08 10:46:32 +0700

    add hh

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,20 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  // ที่ทำหน้าที่ในการบันทึกเงินเดือนของพนักงานหลังจากปรับเพิ่มเงินเดือนแล้ว / ไม่มีการ Return ผลลัพธ์
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i]);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+const newEmployeeSalaries = []; // [25005, 45000, 37000, 19500, 349000]
 
 // Using `forEach` function here
+function addBonus(price) {
+  const newPrice = price + 5000;
+  newEmployeeSalaries.push(newPrice);
+}
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+forEach(employeeSalaries, addBonus);
+console.log(newEmployeeSalaries);
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,41 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  // เอาไว้เช็คจำนวน [i] ที่มีคะแนนมากกว่า 5 จะเป็น true / operation = callbackเรียก arrayที่มีค่ามากกว่า 70
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i]) === true) {
+      count = count + 1;
+    }
+  }
+  return count >= 5;
 }
 
-const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
-const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
-const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67]; // [90,80,100,86,99]
+const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100]; // [78,98,80,100]
+const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67]; // [80,100,80,100]
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+function checkScore(scoreArray) {
+  if (scoreArray > 70) {
+    return true;
+  } else {
+    return false;
+  }
+  /*let count = [];
+  for (let i = 0; i < scoreArray.length; i++) {
+    if (scoreArray[i] > 70) {
+      count.push(scoreArray[i]);
+    }
+  }
+  console.log(count);*/
+}
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScore); // true
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScore); // false
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScore); // false
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
